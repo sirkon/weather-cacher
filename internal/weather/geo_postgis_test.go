3cad7af0368b4ea5c9c0b31f475c56b7a5bf56d9
Denis Cheremisov <denis.cheremisov@gmail.com>
2018-11-11 20:19:49 +0300

    a bit better geo test

--- internal/weather/geo_postgis_test.go
+++ internal/weather/geo_postgis_test.go
@@ -4,7 +4,7 @@
 Используем этот docker-образ: https://hub.docker.com/r/kartoza/postgis/
 После установки запускаем образ и создаём БД data:
 
-    sudo docker run --name "postgis" -p 25432:5432 -d -t kartoza/postgis
+    .
 
     psql -h localhost -U docker -p 25432 -d gis
     CREATE DATABASE data;
@@ -46,34 +46,33 @@ func TestGeoPostgis(t *testing.T) {
 		}
 	)
 
-	data := []node{
-		{
-			id:  "moscow-1",
-			lat: 55.751944,
-			lon: 37.615555,
-		},
-		{
-			id:  "moscow-2",
-			lat: 55.752,
-			lon: 37.61555,
-		},
-		{
-			id:  "leningrad-1",
-			lat: 59.9386111,
-			lon: 30.313888,
-		},
-		{
-			id:  "leningrad-2",
-			lat: 59.94,
-			lon: 30.31389,
-		},
+	moscow1 := node{
+		id:  "moscow-1",
+		lat: 55.751944,
+		lon: 37.615555,
 	}
+	moscow2 := node{
+		id:  "moscow-2",
+		lat: 55.752,
+		lon: 37.61555,
+	}
+	leningrad1 := node{
+		id:  "leningrad-1",
+		lat: 59.9386111,
+		lon: 30.313888,
+	}
+	leningrad2 := node{
+		id:  "leningrad-2",
+		lat: 59.94,
+		lon: 30.31389,
+	}
+
 	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancelFunc()
 
 	_, err = db.ExecContext(ctx,
-		`DELETE FROM forecast WHERE id IN ($1, $2, $3, $4)`,
-		data[0].id, data[1].id, data[2].id, data[3].id,
+		`DELETE FROM forecast WHERE id IN ($1, $2)`,
+		moscow1.id, leningrad1.id,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -83,29 +82,29 @@ func TestGeoPostgis(t *testing.T) {
 
 	const providerID = "prov-id"
 
-	if err := g.Set(ctx, providerID, data[0].lat, data[0].lon, data[0].id); err != nil {
+	if err := g.Set(ctx, providerID, moscow1.lat, moscow1.lon, moscow1.id); err != nil {
 		t.Fatal(err)
 	}
-	if err := g.Set(ctx, providerID, data[2].lat, data[2].lon, data[2].id); err != nil {
+	if err := g.Set(ctx, providerID, leningrad1.lat, leningrad1.lon, leningrad1.id); err != nil {
 		t.Fatal(err)
 	}
 
-	resp, err := g.GetNearby(ctx, providerID, data[1].lat, data[1].lon)
+	resp, err := g.GetNearby(ctx, providerID, moscow2.lat, moscow2.lon)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if _, ok := resp[data[0].id]; !ok {
-		t.Fatalf("there should be %s key in the response", data[0].id)
+	if _, ok := resp[moscow1.id]; !ok {
+		t.Fatalf("there should be %s key in the response", moscow1.id)
 	}
 	require.Len(t, resp, 1)
 	t.Log(litter.Sdump(resp))
 
-	resp, err = g.GetNearby(ctx, providerID, data[3].lat, data[3].lon)
+	resp, err = g.GetNearby(ctx, providerID, leningrad2.lat, leningrad2.lon)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if _, ok := resp[data[2].id]; !ok {
-		t.Fatalf("there should be %s key in the response", data[2].id)
+	if _, ok := resp[leningrad1.id]; !ok {
+		t.Fatalf("there should be %s key in the response", leningrad1.id)
 	}
 	require.Len(t, resp, 1)
 	t.Log(litter.Sdump(resp))
